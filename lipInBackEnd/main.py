df0924dc619b6ae72c0c981bffd0eacc6d651d13
DEV-HUSTLING <anushkadevg@gmail.com>
2026-01-05 17:30:06 -0500

    Fixing render

--- lipInBackEnd/main.py
+++ lipInBackEnd/main.py
@@ -10,7 +10,7 @@ from playwright.sync_api import sync_playwright
 
 load_dotenv()
 app = FastAPI()
-client = OpenAI(api_key=os.getenv("OPENAI_API"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 class CommentsBody(BaseModel):
     post: str
