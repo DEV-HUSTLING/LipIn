6175cbd29ec009c04f8fb40367965c2d6791d65b
DEV-HUSTLING <anushkadevg@gmail.com>
2026-01-19 21:48:12 -0500

    fixing dependency for deployment api changed

--- lipIn/src/auth/authAPI.js
+++ lipIn/src/auth/authAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
     
 export const authAPI = {
   async googleSignIn(profileURL) {
-    const response = await axios.post(`http://127.0.0.1:8000/signin`, {
+    const response = await axios.post(`https://lipin.onrender.com/signin`, {
       profileURL:profileURL
     });
     return response.data;
