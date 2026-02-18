a39e4ede02acd7f835efbfacdfe71ba196d52cb9
DEV-HUSTLING <anushkadevg@gmail.com>
2026-02-17 21:28:49 -0500

    fixing build issue

--- lipInBackEnd/build.sh
+++ lipInBackEnd/build.sh
@@ -7,5 +7,8 @@ set -o errexit  # Exit on error
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Set Playwright browser path to project directory for persistence
+export PLAYWRIGHT_BROWSERS_PATH=$PWD/ms-playwright
+
 # Install Playwright browsers and system dependencies
 playwright install --with-deps chromium
